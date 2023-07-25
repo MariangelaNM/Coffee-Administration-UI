e668f8e9f1b0d9ad0133bfa4fd5d358863cadf1f
Mariángela Navarro <mariangelanavarro10@gmail.com>
2023-07-24 19:19:00 -0600

    Añadir recoleccion, primera verción front

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.scss';
-
 import { FC } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LandingPage from "./routes/LandingPage";
-import Dashboard from "./routes/Dashboard";
-import Admin from "./routes/Admin";
 import UserRegister from "./routes/UserRegister";
 import FarmCreate from './routes/FarmCreate';
 import RecolectorCreate from './routes/RecolectorCreate';
-
-
+import RecoleccionCreate from './routes/RecoleccionCreate';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.scss'
+import Login from './routes/Login';
 
 const App: FC = () => {
     return (
@@ -26,6 +21,7 @@ const App: FC = () => {
                 <Route path="/signup"> <UserRegister /> </Route>  
                 <Route path="/farms" > <FarmCreate/></Route>
                 <Route path="/collector" > <RecolectorCreate/></Route>
+                <Route path="/Recoleccion" > <RecoleccionCreate/></Route>
                 <Route path="/Mis Recolectores" ></Route>
                 <Route path="/Resumen Recolectores" ></Route>
                 <Route path="/Resumen Fincas" ></Route>
